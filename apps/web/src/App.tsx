2226faaeee89011630a9438170aeb202b25f6348
Shadypio <enzuccioamendola@gmail.com>
2026-09-05 23:25:36 +0200

    feat: replace SpeedInsights with Analytics for performance tracking

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,15 +1,14 @@
 import { RouterProvider } from 'react-router-dom';
 import { AppProviders } from './app/providers';
 import { router } from './app/router';
-import { SpeedInsights } from '@vercel/speed-insights/react';
-
+import { Analytics } from '@vercel/analytics/react';
 function App() {
   return (
     <>
       <AppProviders>
         <RouterProvider router={router} />
       </AppProviders>
-      <SpeedInsights />
+      <Analytics />
     </>
   );
 }
